d0fe3fa612a4fc8fd328def03b3ab657f2483be8
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-03-13 17:40:26 +0100

    Repo Api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects= Project::paginate(3);
+        return response()->json([
+            'success'=>true,
+            'results'=>$projects,
+        ]);
+    }
+    public function show(string $slug){
+        $projects= Project::where('slug', $slug)->firstOrFail();
+        return response()->json([
+            'success'=>true,
+            'results'=>$projects,
+        ]);
+    }
+}
