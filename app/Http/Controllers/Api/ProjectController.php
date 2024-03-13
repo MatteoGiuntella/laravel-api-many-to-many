b554143321a3b2cb409771f2e580ea7e09ec4370
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-03-13 18:25:48 +0100

    fix controller api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects= Project::paginate(3);
+        $projects= Project::with('Type','Technologies')->paginate(3);
         return response()->json([
             'success'=>true,
             'results'=>$projects,
